1aadd8369a5309e6410abcd35fe5063cc12ed0c5
Oleksiy Golovko <alexei.golovko@gmail.com>
2017-02-13 12:09:19 +0100

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,10 @@
+module Main exposing (..)
+
+import Html
+import Message
+
+
+--main : Program Never Html.Html Msg
+main =
+    Html.text
+        Message.text
---
+++ src/Message.elm
@@ -0,0 +1,3 @@
+module Message exposing (..)
+
+text = "New Text"
