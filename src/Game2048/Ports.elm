528dc04cbcba62caff67894ed2c422dec0290c70
Oleksiy Golovko <alexei.golovko@gmail.com>
2017-02-22 16:25:32 +0100

    create initial page

--- src/Game2048/Ports.elm
+++
@@ -1,9 +0,0 @@
-port module Game2048.Ports exposing (size, save)
-
-import Game2048.Model exposing (Size, TileData)
-
-
-port size : (Size -> msg) -> Sub msg
-
-
-port save : (Int,  Int, List TileData ) -> Cmd msg
